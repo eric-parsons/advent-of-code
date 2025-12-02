f2cc15ac75dbe41af420428103310e8ff033f555
Eric Parsons <eric.d.parsons@protonmail.com>
2025-12-02 09:08:24 -0600

    Adding more helpful annotations to the solution for day 1.

--- 2025/Day1.hs
+++ 2025/Day1.hs
@@ -1,29 +1,38 @@
 -- See https://adventofcode.com/2025/day/1.
 -- Assumes valid input. Focus is on brevity and simplicity over optimization.
 
+-- A position on the dial.
+type Position = Int
+
+-- An amount to rotate by. Negative values move left, positive moves right.
+type Rotation = Int
+
 main = do
   input <- readFile "./Day1.input.txt"
   let rotations = parseInput input
   print $ part1 rotations
   print $ part2 rotations
 
+part1 :: [Rotation] -> Int
 part1 = length . filter (== 0) . positions
 
+part2 :: [Rotation] -> Int
 part2 rs = sum $ zipWith countZeroes (positions rs) rs
   where
     countZeroes pos rotation
+      -- Right rotation.
       | rotation >= 0 = (pos + rotation) `div` 100
+      -- Left rotation. Invert the logic into the equivalent right rotation.
       | otherwise = (((100 - pos) `mod` 100) - rotation) `div` 100
 
 -- Takes a list of rotations and produces a list of cumulative positions after
 -- each rotation.
-positions :: [Int] -> [Int]
+positions :: [Rotation] -> [Position]
 positions = scanl turn 50
   where
     turn pos rotation = (pos + rotation) `mod` 100
 
--- Left rotations are negative and right rotations are positive.
-parseInput :: String -> [Int]
+parseInput :: String -> [Rotation]
 parseInput = map readRotation . lines
   where
     readRotation ('L' : n) = -read n
